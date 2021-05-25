759277bc13ee0aaf006e374358c96e5365c1ab39
Magnido9 <40192962+Magnido9@users.noreply.github.com>
2021-05-25 15:27:39 +0300

    Add files via upload

--- OOP3/OOP/Solution/Injector.java
+++ OOP3/OOP/Solution/Injector.java
@@ -16,9 +16,6 @@ import java.util.HashMap;
 import java.util.function.Supplier;
 
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.METHOD,ElementType.CONSTRUCTOR})
- @interface Named {}
 public class Injector {
     HashMap<Class,Class> classBinds;
     HashMap<String,Class> nameBinds;
@@ -36,45 +33,48 @@ public class Injector {
         else
             throw new IllegalBindException();
     }
-    public void bindToInstance(Class clazz1, Object clazz2) throws IllegalBindException{
-        if(clazz1.isAssignableFrom(clazz2.getClass()))
-            instanceBinds.put(clazz1,clazz2);
+    public void bindToInstance(Class clazz1, Object instance) throws IllegalBindException{
+        if(instance==null)
+            throw new IllegalBindException();
+        if(clazz1.isAssignableFrom(instance.getClass()))
+            instanceBinds.put(clazz1,instance);
         else
             throw new IllegalBindException();
     }
-    public void bindToName(Class clazz1, String clazz2) throws IllegalBindException{
-        if(clazz2.getClass().equals(clazz1))
-            nameBinds.put(clazz2,clazz1);
-        else
-            throw new IllegalBindException();
+    public void bindByName( String clazz2,Class clazz1) throws IllegalBindException{
+        nameBinds.put(clazz2,clazz1);
     }
     public void bindToSupplier(Class clazz1, Supplier clazz2) throws IllegalBindException{
-        if(clazz2.getClass().equals(clazz1))
+        if(clazz2.get().getClass().equals(clazz1))
             supplierBinds.put(clazz1,clazz2);
         else
             throw new IllegalBindException();
     }
     public Object construct(Class clazz) throws MultipleInjectConstructorsException, NoConstructorFoundException, MultipleAnnotationOnParameterException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        if(clazz.getConstructors().length<1)
-            throw new NoConstructorFoundException();
+        if(instanceBinds.containsKey(clazz))
+            return instanceBinds.get(clazz);
         else if(classBinds.containsKey(clazz))
             return construct(classBinds.get(clazz));
-        else if(instanceBinds.containsKey(clazz))
-            return instanceBinds.get(clazz);
         else if(supplierBinds.containsKey(clazz))
             return supplierBinds.get(clazz).get();
         else if(Arrays.stream(clazz.getConstructors()).filter(a->a.isAnnotationPresent(Inject.class)).toArray().length>1)
             throw new MultipleInjectConstructorsException();
         else if(Arrays.stream(clazz.getConstructors()).filter(a->a.isAnnotationPresent(Inject.class)).toArray().length==0) {
-            Object o=Arrays.stream(clazz.getConstructors()).filter(a -> a.getParameters().length == 0).toList().get(0).newInstance();
+            if(Arrays.stream(clazz.getConstructors()).filter(a -> a.getParameters().length == 0).toArray().length==0)
+                throw new NoConstructorFoundException();
+            Constructor m=Arrays.stream(clazz.getConstructors()).filter(a -> a.getParameters().length == 0).toList().get(0);
+            m.setAccessible(true);
+            Object o=m.newInstance();
             return o;
             } else {
                 Constructor c= Arrays.stream(clazz.getConstructors()).filter(a -> a.isAnnotationPresent(Inject.class)).toList().get(0);
                 Object[] parameters=constructParameters(c.getParameters());
+                c.setAccessible(true);
                 Object o=c.newInstance(parameters);
 
                 Arrays.stream(clazz.getDeclaredMethods()).filter(a-> a.isAnnotationPresent(Inject.class)).forEach(a-> {
                     try {
+                        a.setAccessible(true);
                         a.invoke(o ,constructParameters(a.getParameters()));
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
@@ -111,10 +111,13 @@ public class Injector {
 
     private Object[] constructParameters(Parameter[] parameters) throws MultipleInjectConstructorsException, NoConstructorFoundException, MultipleAnnotationOnParameterException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Object[] constructed_parameters=new Object[parameters.length];
+        Named na;
         int c=0;
         for (Parameter parameter:parameters) {
-            if(parameter.isAnnotationPresent(Named.class) && nameBinds.containsKey(parameter.getName()))
-               constructed_parameters[c]=construct(nameBinds.get(parameter.getName()));
+            if(parameter.isAnnotationPresent(Named.class) && nameBinds.containsKey(parameter.getName())){
+                na=(Named) Arrays.stream(parameter.getAnnotations()).filter(a->a.annotationType()==Named.class).toList().get(0);
+                constructed_parameters[c]=construct(nameBinds.get(na.value()));
+            }
             else if(parameter.getAnnotations().length>0 && !parameter.isAnnotationPresent(Named.class))
                 constructed_parameters[c]=provides(parameter, Arrays.stream(parameter.getAnnotations()).toList().get(0));
             else
